1f1ebf8876d3b1a08e6a931931a5a95da489084d
gui vieira <guivieirasilva02@gmail.com>
2022-08-22 11:32:50 -0300

    finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
+import { Outlet } from 'react-router-dom'
 
-import { Link, Outlet } from 'react-router-dom'
+import Navbar from "./components/Navbar"
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      <nav id='nav.bar'>
-        <h2>
-          <Link to="/">MoviesLib</Link>
-        </h2>
-        <Link to="/movie/1">Movie</Link>
-        <Link to="/search">search</Link>
-      </nav>
-      <h2>MoviesLib</h2>
+      <Navbar />
       <Outlet />
     </div>
   )
