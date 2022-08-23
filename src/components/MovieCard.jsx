69537e2df7611a32b56d58d1979f97354886912d
gui vieira <guivieirasilva02@gmail.com>
2022-08-23 08:57:27 -0300

    styling the cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,6 +7,7 @@ const imageUrl = import.meta.env.VITE_IMG
 function MovieCard({movie, showLink = true}){
    return(
       <div className="movie-card" >
+         
          <img src={imageUrl + movie.poster_path} alt={movie.title}></img>
          <h2>{movie.title}</h2>
          <p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import MovieCard from "../components/MovieCard"
 
+import "./moviesGrid.css"
+
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
 
@@ -25,8 +27,8 @@ function Home() {
     <div className="container">
       <h2 className="title">Melhores Filmes</h2>
       <div className="movies-container">
-      {topMovies.length === 0 && <p>Carregando...</p>}
-      {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   )
